c08397976ec8e465bfba120b3d0ed4fcec0abaa1
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2024-09-28 21:41:52 +0200

    Added SporadicTrait

---
+++ PvZHMod/StatusEffectSporadicTrait.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+using System.Collections;
+// HUGE Credits to Michael C. Without them, Overshoot wouldn't exist.
+
+public class StatusEffectSporadicTrait : StatusEffectTemporaryTrait
+{
+	private bool active = true;
+	public override void Init()
+	{
+		base.OnCardPlayed += (_,__) => { return Deactivate(); };
+        base.OnTurnEnd += (_) => { return Activate(); };
+        base.Init();
+	}
+
+    public IEnumerator Activate()
+    {
+        if (!active)
+        {
+            active = true;
+            yield return base.StackRoutine(count);
+        }
+    }
+
+    public IEnumerator Deactivate()
+    {
+        if (active)
+        {
+            active = false;
+            yield return base.EndRoutine();
+        }
+    }
+
+    public override bool RunTurnEndEvent(Entity entity)
+    {
+        return (entity?.data?.cardType?.name == "Leader");
+    }
+
+    public override bool RunCardPlayedEvent(Entity entity, Entity[] targets)
+    {
+        return (entity == target);
+    }
+}
